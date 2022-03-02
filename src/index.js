4bf5c6a3e26be1943969a65f53b595e5f7dfca0d
Rosie Gardner <rosiegardner78@gmail.com>
2022-03-02 14:25:35 -0800

    update sorce code triangle.js, passes 1st test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import Triangle from './triangle.js';
+
 $(document).ready(function() {
   $('#triangle-checker-form').submit(function(event) {
     event.preventDefault();
